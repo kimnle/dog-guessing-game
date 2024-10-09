e19863375e48ba7ba8374397b44a0ded58ed2283
Kim <72332310+kimnle@users.noreply.github.com>
2024-10-09 16:13:34 +1100

    fetch data from dog api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
+
 function App() {
+  // fetch data when the app first renders using useEffect
+  useEffect(() => {
+    // load the json from the dog api url using the async await syntax
+    async function go() {
+        const picsPromise = await fetch("https://dog.ceo/api/breeds/image/random/50");
+        const pics = await picsPromise.json();
+        console.log(pics.message);
+    }
+    // right after the definition call it imediately
+    go()
+  }, [])
+
   return (
     <div>
-      <h1 className="text-indigo-500 font-bold text-3xl">Hello from React</h1>
+      <h1 className="text-green-500 font-bold text-3xl">Hello from React</h1>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App"
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("app")).render(
+  // strict and development mode calls useEffect twice
   <React.StrictMode>
     <App />
   </React.StrictMode>
