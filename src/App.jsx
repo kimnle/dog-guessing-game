870bc62e657e9973132752b18d49512fc0722183
Kim <72332310+kimnle@users.noreply.github.com>
2024-10-09 20:58:50 +1100

    get data ready for questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
 import { useEffect } from "react";
 
+// function to filter out duplicates and shave it off so that the array ends in a multiple of four
+function onlyUniqueBreeds(pics) {
+  const uniqueBreeds = [];
+  // loop through the array of urls and build a list of all unique breeds
+  const uniquePics = pics.filter(pic => {
+    // figure out what the breed of the current dog is
+    const breed = pic.split("/")[4];
+    // if return true the current item that's been looped to will be included in the new array that's building
+    // if return false it will be skipped
+
+    // if the breed doesn't already exist
+    // in the list of breeds that we're keeping
+    // track of and if it doesn't include a space
+    // in the file name (don't want to bother converting
+    // the space into %20) then include it in the new array
+    // that's building
+    if (!uniqueBreeds.includes(breed) && !pic.includes(" ")) {
+      // add it to collection that's been kept track of
+      uniqueBreeds.push(breed);
+      return true;
+    }
+  });
+  // return a multiple of four to guarantee there won't be any duplicate breeds when the next 50s are combined
+  return uniquePics.slice(0, Math.floor(uniquePics.length / 4) * 4);
+}
+
 function App() {
   // fetch data when the app first renders using useEffect
   useEffect(() => {
@@ -12,7 +38,8 @@ function App() {
         // use reqController when performing the fetch to identify this request and cancel it in clean up function
         const picsPromise = await fetch("https://dog.ceo/api/breeds/image/random/50", {signal: reqController.signal});
         const pics = await picsPromise.json();
-        console.log(pics.message);
+        const uniquePics = onlyUniqueBreeds(pics.message);
+        console.log(uniquePics);
       } catch {
         console.log("Our request was cancelled");
       }
